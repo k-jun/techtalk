abbad1e797c92e0087b67f297a4075faf9bd9e97
k-jun <keijun.kumagai@dena.jp>
2020-06-01 02:18:54 +0900

    feat(mysql): create integration test

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -2,6 +2,8 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
+	"strconv"
 	"techtalk/models"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -9,34 +11,103 @@ import (
 
 type IMySQL interface {
 	GetChannelMessage(cid string) ([]models.Message, error)
-	CreateChannelMessage(cid string, m *models.Message) error
-	UpdateChannelMessage(cid string, m *models.Message) error
-	DeleteChannelMessage(cid string, mid string) error
+	CreateChannelMessage(m *models.Message) error
+	UpdateChannelMessage(m *models.Message) error
+	DeleteChannelMessage(mid string) error
 }
 
 type sMySQL struct {
 	db *sql.DB
 }
 
-func NewSMySQL(conn *sql.DB) IMySQL {
-	return &sMySQL{
-		db: conn,
+func NewSMySQL(conn *sql.DB) (IMySQL, error) {
+	if err := conn.Ping(); err != nil {
+		return nil, err
 	}
+
+	return &sMySQL{db: conn}, nil
 }
 
 func (m *sMySQL) GetChannelMessage(channelID string) ([]models.Message, error) {
 	messages := make([]models.Message, 0)
+	rows, err := m.db.Query(`
+		SELECT id, channel_id, user_id, type, body, created_at, updated_at
+		FROM messages as m
+		WHERE m.channel_id = ?`,
+		channelID,
+	)
+	if err != nil {
+		return messages, err
+	}
+
+	for rows.Next() {
+		me := models.Message{}
+		err = rows.Scan(&me.ID, &me.ChannelID, &me.UserID, &me.Type, &me.Body, &me.CreatedAt, &me.UpdatedAt)
+		if err != nil {
+			return messages, err
+		}
+		messages = append(messages, me)
+	}
+
 	return messages, nil
 }
 
-func (m *sMySQL) CreateChannelMessage(channelID string, message *models.Message) error {
+func (m *sMySQL) CreateChannelMessage(message *models.Message) error {
+
+	result, err := m.db.Exec(`
+		INSERT INTO messages(id, channel_id, user_id, type, body, created_at, updated_at)
+		VALUES(NULL, ?, ?, ?, ?, ?, ?);`,
+		message.ChannelID, message.UserID, message.Type, message.Body, message.CreatedAt, message.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+	lastID, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	message.ID = strconv.FormatInt(lastID, 10)
 	return nil
 }
 
-func (m *sMySQL) UpdateChannelMessage(channelID string, message *models.Message) error {
+func (m *sMySQL) UpdateChannelMessage(message *models.Message) error {
+	result, err := m.db.Exec(`
+		UPDATE messages as m 
+		SET type = ?,
+			body = ?,
+			updated_at = ?,
+			created_at = ?
+		WHERE m.id = ?;`,
+		message.Type, message.Body, message.CreatedAt, message.UpdatedAt, message.ID,
+	)
+	if err != nil {
+		return err
+	}
+	num, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if num != 1 {
+		return errors.New("message not found")
+	}
 	return nil
 }
 
-func (m *sMySQL) DeleteChannelMessage(channelID string, messageID string) error {
+func (m *sMySQL) DeleteChannelMessage(messageID string) error {
+	result, err := m.db.Exec(`
+		DELETE FROM messages
+		WHERE messages.id = ?;`,
+		messageID,
+	)
+	if err != nil {
+		return err
+	}
+	num, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if num != 1 {
+		return errors.New("message not found")
+	}
 	return nil
 }
--- mysql/mysql_integration_test.go
+++ mysql/mysql_integration_test.go
@@ -5,14 +5,110 @@ package mysql
 import (
 	"database/sql"
 	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"techtalk/models"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var m IMySQL
+var db IMySQL
 
-func TestMain(t *testing.M) {
+func TestMain(m *testing.M) {
+	dbusername := "root"
+	dbpassword := "password1!"
+	dbname := "mysqldb"
 	dbinfo := fmt.Sprintf("%s:%s@/%s", dbusername, dbpassword, dbname)
-	conn, err := sql.Open(dbinfo)
+	conn, err := sql.Open("mysql", dbinfo)
+	if err != nil {
+		panic(err)
+	}
+	db, err = NewSMySQL(conn)
+	if err != nil {
+		panic(err)
+	}
+	os.Exit(m.Run())
+}
+
+func TestGetChannelMessage(t *testing.T) {
+	randomID := rand.Intn(100)
+	messages, err := db.GetChannelMessage(strconv.Itoa(randomID))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(messages) < 100 {
+		t.Fatal("result row is too little")
+	}
+}
+
+func TestCreateChannelMessage(t *testing.T) {
+	me := models.Message{
+		ChannelID: "1",
+		UserID:    "1",
+		Type:      "test-type",
+		Body:      "test-body",
+		CreatedAt: 1561939200,
+		UpdatedAt: 1561939200,
+	}
+
+	err := db.CreateChannelMessage(&me)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if me.ID == "" {
+		t.Fatal("new message may not be inserted")
+	}
+}
+
+func TestUpdateChannelMessage(t *testing.T) {
+	me := models.Message{
+		ChannelID: "1",
+		UserID:    "1",
+		Type:      "test type",
+		Body:      "test body",
+		CreatedAt: 1561939200,
+		UpdatedAt: 1561939200,
+	}
+	err := db.CreateChannelMessage(&me)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if me.ID == "" {
+		t.Fatal("new message may not be inserted")
+	}
+
+	me.Type = "updated body"
+	me.Body = "updated body"
+	me.CreatedAt = 1561939201
+	me.UpdatedAt = 1561939202
+
+	err = db.UpdateChannelMessage(&me)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDeleteChannelMessage(t *testing.T) {
+	me := models.Message{
+		ChannelID: "1",
+		UserID:    "1",
+		Type:      "test type",
+		Body:      "test body",
+		CreatedAt: 1561939200,
+		UpdatedAt: 1561939200,
+	}
+	err := db.CreateChannelMessage(&me)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if me.ID == "" {
+		t.Fatal("new message may not be inserted")
+	}
+
+	err = db.DeleteChannelMessage(me.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
