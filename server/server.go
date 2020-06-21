fa1ccb9b1898119ffd3070e5b61e3f0c011aaa49
keijun.kumagai <keijun091221@gmail.com>
2020-06-21 15:00:56 +0900

    feat(server): set timeout context

--- server/server.go
+++ server/server.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	"context"
 	"database/sql"
 	"net/http"
 	"techtalk/controllers"
 	"techtalk/mysql"
 	rds "techtalk/redis"
+	"time"
 
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
@@ -24,6 +26,7 @@ func NewServer(db *sql.DB, rc *redis.Client) http.Server {
 		panic(err)
 	}
 	attachHandlers(router, mysqldb, rds)
+	attachHandlersWithContext(router, mysqldb, rds)
 
 	s := http.Server{
 		Addr:    ":8000",
@@ -34,7 +37,33 @@ func NewServer(db *sql.DB, rc *redis.Client) http.Server {
 
 func attachHandlers(mux *mux.Router, db mysql.IMySQL, rds rds.IRedis) {
 	mux.HandleFunc("/channels/{id}/messages", controllers.GetMessages(db, rds)).Methods(http.MethodGet)
-	mux.HandleFunc("/channels/{id}/messages", controllers.PostMessage(db, rds)).Methods(http.MethodPost)
-	mux.HandleFunc("/channels/{id}/messages", controllers.PutMessage(db, rds)).Methods(http.MethodPut)
-	mux.HandleFunc("/channels/{id}/messages", controllers.DeleteMessage(db, rds)).Methods(http.MethodDelete)
+}
+
+func attachHandlersWithContext(mux *mux.Router, db mysql.IMySQL, rds rds.IRedis) {
+	muxTimeout := mux.PathPrefix("").Subrouter()
+	muxTimeout.Use(WithTimeout)
+	muxTimeout.HandleFunc("/channels/{id}/messages", controllers.PostMessage(db, rds)).Methods(http.MethodPost)
+	muxTimeout.HandleFunc("/channels/{id}/messages", controllers.PutMessage(db, rds)).Methods(http.MethodPut)
+	muxTimeout.HandleFunc("/channels/{id}/messages", controllers.DeleteMessage(db, rds)).Methods(http.MethodDelete)
+}
+
+func WithTimeout(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+
+		ch := make(chan string)
+
+		go func() {
+			next.ServeHTTP(w, r)
+			ch <- "done"
+		}()
+		select {
+		case <-ch:
+			// complete before timeout
+		case <-ctx.Done():
+			// not complete within 100ms
+		}
+		return
+	})
 }
